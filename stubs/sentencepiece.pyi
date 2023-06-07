1a88d9782eefd01296d26542c16df043d48f5b52
Gabriel Chaperon <gabrielchaperonb@gmail.com>
2023-06-07 00:29:41 -0400

    Feat/data loading (#9)
    * Add project dependencies
    
    * Add data loaders
    
    * Update typing dependencies
    
    * Use toch for cpu only in CI.

--- stubs/sentencepiece.pyi
+++ stubs/sentencepiece.pyi
@@ -4,11 +4,51 @@ The Python version of this package is poorly documented, so I'm trying to do
 what I can using the examples
 [here](https://github.com/google/sentencepiece/tree/master/python).
 """
-from typing import BinaryIO, Iterator
+from typing import BinaryIO, Iterator, overload
 
 class SentencePieceProcessor:
     def __init__(self, model_file: str, num_threads: int = -1) -> None: ...
     def id_to_piece(self, id: int) -> str: ...
+    @overload
+    def encode(
+        self,
+        input: str,
+        out_type: type[int] | None = None,
+        add_bos: bool = False,
+        add_eos: bool = False,
+        revers: bool = False,
+        num_threads: int = -1,
+    ) -> list[int]: ...
+    @overload
+    def encode(
+        self,
+        input: list[str],
+        out_type: type[int] | None = None,
+        add_bos: bool = False,
+        add_eos: bool = False,
+        revers: bool = False,
+        num_threads: int = -1,
+    ) -> list[list[int]]: ...
+    @overload
+    def encode(
+        self,
+        input: str,
+        out_type: type[str],
+        add_bos: bool = False,
+        add_eos: bool = False,
+        revers: bool = False,
+        num_threads: int = -1,
+    ) -> list[str]: ...
+    @overload
+    def encode(
+        self,
+        input: list[str],
+        out_type: type[str],
+        add_bos: bool = False,
+        add_eos: bool = False,
+        revers: bool = False,
+        num_threads: int = -1,
+    ) -> list[list[int]]: ...
     def get_score(self, id: int) -> float: ...
     def get_piece_size(self) -> int: ...
 
